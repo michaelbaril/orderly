05fc84ad060f6cdbea7cd7df43b810e14bad5749
Michaël Bareyt <michael.bareyt@webedia-group.com>
2020-02-12 14:12:52 +0100

    added tests for console command

---
+++ tests/ConsoleTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Baril\Orderable\Tests;
+
+use Baril\Orderable\Tests\Models\Article;
+use Baril\Orderable\Tests\Models\Tag;
+
+class ConsoleTest extends TestCase
+{
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->article = factory(Article::class)->create();
+        $this->tags = factory(Tag::class, 5)->create();
+        $this->article->tags()->attach(Tag::all());
+    }
+
+    public function test_fix_positions()
+    {
+        $instance = $this->tags[0];
+        $table = $instance->getTable();
+        $connection = $instance->getConnection();
+        $connection->update("UPDATE $table SET position = 10 LIMIT 1");
+
+        $this->assertNotEquals([1, 2, 3, 4, 5], Tag::ordered()->get()->pluck('position')->all());
+        $this->artisan('orderable:fix-positions '.addslashes(Tag::class));
+        $this->assertEquals([1, 2, 3, 4, 5], Tag::ordered()->get()->pluck('position')->all());
+    }
+
+    public function test_fix_relation_positions()
+    {
+        $table = $this->article->tags()->getTable();
+        $connection = $this->article->getConnection();
+        $connection->update("UPDATE $table SET position = 10 LIMIT 1");
+
+        $this->assertNotEquals([1, 2, 3, 4, 5], $this->article->tags()->ordered()->get()->pluck('pivot.position')->all());
+        $this->artisan('orderable:fix-positions '.addslashes(Article::class). ' tags');
+        $this->assertEquals([1, 2, 3, 4, 5], $this->article->tags()->ordered()->get()->pluck('pivot.position')->all());
+    }
+}
