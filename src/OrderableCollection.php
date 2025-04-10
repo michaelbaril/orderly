c66fff6e1aa246e68d9144c70d01facfd040182c
Michaël <michbare@yahoo.fr>
2025-04-10 22:15:04 +0200

    Revert "Revert "Revert "Revert "Revert "trigger CI"""""
    This reverts commit 4e5b39672676d4f2c283032af9f35304b81fc964.

--- src/OrderableCollection.php
+++ src/OrderableCollection.php
@@ -47,7 +47,6 @@ class OrderableCollection extends EloquentCollection
         $count = $this->count();
         $ordered = $this->sortBy(function ($item) use ($ids, $count) {
             $index = array_search($item->getKey(), $ids);
-
             return ($index === false) ? $count + $item->getPosition() : $index;
         });
         return $ordered->saveOrder();
